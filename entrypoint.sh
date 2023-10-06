f41bcc48c5301aeca420681995cf4b6babeed220
LeiCraft_ <linusfischer08@gmx.net>
2023-10-06 20:32:59 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 sleep 1
 
+HOSTS_FILE="/etc/hosts"
+
+# Function to add an entry to the hosts file
+add_host() {
+    local hostname="$1"
+    local ip="$2"
+    echo "$ip $hostname" >> "$HOSTS_FILE"
+}
+
+# Function to remove an entry from the hosts file
+remove_host() {
+    local hostname="$1"
+    sed -i "/\s$hostname$/d" "$HOSTS_FILE"
+}
+
 cd /home/container
 
 # Replace Startup Variables
--- start.sh
+++ start.sh
@@ -8,11 +8,43 @@ if [ ! -f "$cert_dir/fullchain.pem" ] || [ ! -f "$cert_dir/privkey.pem" ] || [ !
     mkdir -p "$cert_dir"
 
     echo "ℹ️ Generating SSL certificate for $server_name..."
-    certbot --nginx -d "$server_name" --cert-path "$cert_dir/fullchain.pem" --key-path "$cert_dir/privkey.pem" --work-dir /home/container/tmp --logs-dir /home/container/tmp
+    certbot certonly --standalone -d "$server_name" --cert-path "$cert_dir/fullchain.pem" --key-path "$cert_dir/privkey.pem" --config-dir "$cert_dir" --work-dir /home/container/logs --logs-dir /home/container/logs --register-unsafely-without-email --non-interactive --agree-tos
 
     echo "⟳ Restarting Nginx to apply SSL certificate..."
     /usr/sbin/nginx -s reload
 fi
+
+# Define the file to store the last run timestamp
+timestamp_file="$cert_dir/last_run_timestamp.txt"
+
+# Define the cron expression for running once a month
+cron_expression="0 0 1 * *"
+
+# Get the current timestamp
+current_timestamp=$(date +%s)
+
+# Initialize the last run timestamp if it doesn't exist
+if [ ! -f "$timestamp_file" ]; then
+    echo "0" > "$timestamp_file"
+fi
+
+# Read the last run timestamp from the file
+last_run_timestamp=$(cat "$timestamp_file")
+
+# Calculate the time elapsed since the last run
+time_elapsed=$((current_timestamp - last_run_timestamp))
+
+# Check if it's been at least a month since the last run
+if [ "$time_elapsed" -ge 2592000 ]; then
+    # Execute Certbot to renew certificates
+    certbot renew
+
+    # Update the last run timestamp in the file
+    echo "$current_timestamp" > "$timestamp_file"
+else
+    echo "Certificates were renewed within the last month. Skipping."
+fi
+
 
 rm -rf /home/container/tmp/*
 
