2f2ab188eb52d5a6247b715a3e4170b022218752
LeiCraft_ <linusfischer08@gmx.net>
2023-10-07 16:39:41 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,25 @@
-FROM alpine:latest
+# Use the official PHP 8.1 Apache image as the base image
+FROM php:8.1-apache
 
-RUN apk --update --no-cache add curl ca-certificates nginx
+RUN apt-get update && apt-get install -y git
 
-RUN apk add --no-cache sudo
-
-FROM php:8.0-alpine
-RUN apk add php8 php8-xml php8-exif php8-fpm php8-session php8-soap php8-openssl php8-gmp php8-pdo_odbc php8-json php8-dom php8-pdo php8-zip php8-mysqli php8-sqlite3 php8-pdo_pgsql php8-bcmath php8-gd php8-odbc php8-pdo_mysql php8-pdo_sqlite php8-gettext php8-xmlreader php8-bz2 php8-iconv php8-pdo_dblib php8-curl php8-ctype php8-phar php8-fileinfo php8-mbstring php8-tokenizer php8-simplexml
-COPY --from=composer:latest  /usr/bin/composer /usr/bin/composer
-
-RUN apk add certbot
-
-RUN chmod 777 /etc/hosts
-
-USER container
-ENV  USER container
-ENV HOME /home/container
-
+# Set the working directory to /home/container
 WORKDIR /home/container
-COPY ./entrypoint.sh /entrypoint.sh
 
-CMD ["/bin/ash", "/entrypoint.sh"]
+Copy . /home/container
+
+# Copy the Apache configuration file to the home directory
+
+# Disable the default virtual host
+RUN a2dissite 000-default
+
+# Expose a placeholder port (you can use any unused port)
+EXPOSE 12200
+
+RUN sed -i "s/Listen 80/Listen 12200/" /etc/apache2/ports.conf
+
+#RUN chmod 777 /etc/apache2/ports.conf
+
+# Define an entry point script to start Apache with the configured port and config
+Copy start-apache.sh /start-apache.sh
+CMD ["/bin/bash", "/start-apache.sh"]
---
+++ start-apache.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+git
+echo "Server starting..."
+
+# Function to start Apache server
+start_apache() {
+    # Use the provided port or prompt for one if empty
+    cd /home/container
+    # Symlink the Apache configuration file from the home directory
+    ln -s /home/container/.apache/site-config.conf /etc/apache2/sites-available/leicoin-node.conf
+    # Enable the site configuration
+    a2ensite leicoin-node
+
+    echo "Server Stardet"
+
+    # Start the Apache server in the background
+    apache2-foreground
+}
+
+# Function to stop Apache server
+stop_apache() {
+    # Stop the Apache server
+    apachectl stop
+}
+
+# Trap SIGINT to prevent termination with Ctrl+C
+#trap 'echo "Use 'exit' to quit."' INT
+
+# Initialize variables to hold silent mode and remaining arguments
+SILENT_MODE=false
+REMAINING_ARGS=()
+
+# Loop through command-line arguments
+for arg in "$@"; do
+    if [ "$arg" == "--silent-start" ]; then
+        SILENT_MODE=true
+    else
+        REMAINING_ARGS+=("$arg")
+    fi
+done
+
+# Prompt the user for action (start or exit)
+while true; do
+
+    if [ "$SILENT_MODE" == "true" ]; then
+        ACTION="start"
+        break
+    else
+        read -p "Enter 'start' to start Apache, or 'exit' to quit: " ACTION    
+    fi
+
+    if [ "$ACTION" == "start" ]; then
+        start_apache
+        break
+    elif [ "$ACTION" == "exit" ]; then
+        exit 0
+    else
+        echo "Invalid action. Use 'start' or 'exit'."
+    fi
+    # Prompt for additional commands after starting Apache
+    #while [ "$ACTION" == "start" ]; do
+        #read -p "Enter a command (or 'stop' to stop Apache): " COMMAND
+
+        #if [ "$COMMAND" == "stop" ]; then
+            #stop_apache
+            #exit 0
+        #else
+            #echo "Executing command: $COMMAND"
+            #eval $COMMAND
+        #fi
+    #done
+done
+
