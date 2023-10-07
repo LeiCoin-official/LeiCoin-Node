c3464a04c6be18b951ff71221842e352b9725d98
LeiCraft_ <linusfischer08@gmx.net>
2023-10-07 18:06:34 +0200

    update

--- start-apache.sh
+++ start-apache.sh
@@ -9,9 +9,7 @@ else
     rm -R /home/gitrepo
     git clone https://github.com/LeiCraft/LeiCoin-Node.git /home/gitrepo
 fi
-shopt -s dotglob
-cp -r --preserve=all --exclude='\.git*' --exclude='Dockerfile' --exclude='start-apache.sh' /home/gitrepo/* /home/container/
-
+rsync -av --delete --exclude='*\.git*' --exclude='Dockerfile' --exclude='start-apache.sh' /home/gitrepo/ /home/container/
 
 cd /home/container
 
