80188ba4c159bd44e863965f18367a9b95ec11b5
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-08 16:52:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
-const config = require( __dirname + '/config/config.json');
 
+// Define the paths for the configuration files
+const configFilePath = path.join(__dirname, 'config/config.json');
+const defaultConfigFilePath = path.join(__dirname, 'config/sample.config.json');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Function to load the configuration file or a default if it doesn't exist
+function loadConfig() {
+  try {
+    // Check if the configuration file exists
+    if (fs.existsSync(configFilePath)) {
+      // If it exists, read and parse the configuration
+      const configData = fs.readFileSync(configFilePath, 'utf-8');
+      return JSON.parse(configData);
+    } else {
+      // If it doesn't exist, read and parse the default configuration
+      const defaultConfigData = fs.readFileSync(defaultConfigFilePath, 'utf-8');
+      return JSON.parse(defaultConfigData);
+    }
+  } catch (error) {
+    console.error('Error loading configuration:', error);
+    return null;
+  }
+}
 
-app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`);
-});
+// Load the configuration
+const config = loadConfig();
 
+if (config) {
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
+} else {
+  console.error('Failed to load configuration. Exiting...');
+  process.exit(1);
+}
