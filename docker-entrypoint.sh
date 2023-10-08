cd91d7461d32afb02af00f58cde95d47b631668e
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-08 18:30:54 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,18 +22,38 @@ else
     echo "Failed to clone the GitHub repository."
 fi
 
-
 # Remove temporary directories
 rm -rf /home/container/gittmp
 
 # Extract the value of --internal-port from STARTUP if it exists
 internal_port=""
-for arg in "$@"; do
-    if [ "$arg" == "--internal-port" ]; then
-        # Get the port value after --internal-port
-        internal_port="$1"
-        break
-    fi
-done
+
+if [ -n "$STARTUP" ] && [[ "$STARTUP" == "start"* ]]; then
+    # Use parameter expansion to extract the value after --internal-port
+    raw_internal_port="${STARTUP#*--internal-port }"
+    # Replace placeholders in raw_internal_port using eval and sed
+    MODIFIED_STARTUP=$(eval echo $(echo ${raw_internal_port} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+    # Use parameter expansion again to extract the modified value
+    internal_port="${MODIFIED_STARTUP#*--internal-port }"
+fi
+
+# If --internal-port is not found in STARTUP, check regular shell arguments
+if [ -z "$internal_port" ]; then
+    for arg in "$@"; do
+        if [ "$arg" == "--internal-port" ]; then
+            # Get the port value after --internal-port
+            internal_port="$1"
+            break
+        fi
+    done
+fi
+
+# Update the Nginx configuration if internal_port is set
+if [ -n "$internal_port" ]; then
+    sed -i "s/\"port\": [0-9]\+,\"port\": $internal_port,/" /home/container/config/config.json
+
+    echo "Updated config to listen on port $internal_port."
+fi
+
 
 node index.js
