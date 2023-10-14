fbb051e0acb19051928e4c8ebc40ad5959023d38
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-14 18:42:21 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -10,36 +10,33 @@ const data_message = {};
 
 function generateLogMessage(prefix, message, style = 'reset') {
     const styles = {
-      reset: chalk.reset,
-      green: chalk.green,
-      red: chalk.red,
-      blue: chalk.blue,
+        reset: chalk.reset,
+        green: chalk.green,
+        red: chalk.red,
     };
   
     if (styles[style]) {
-      return `${chalk[prefix](`[${prefix}]`)} ${styles[style](message)}`;
+        return `${chalk[style](`${prefix}`)} ${styles[style](message)}`;
     } else {
-      return `${chalk[prefix](`[${prefix}]`)} ${chalk.reset(message)}`;
+        return `${chalk[prefix]} ${chalk.reset(message)}`;
     }
-  }
+}
   
-  const messageTypes = ['log', 'success', 'error'];
+const messageTypes = ['log', 'success', 'error'];
   
-  // Define mappings of prefixes and colors
-  const messageConfigs = [
-    { object: miner_message, prefix: 'Miner', color: 'cyan' },
-    { object: server_message, prefix: 'Server', color: 'magenta' },
-    { object: data_message, prefix: 'Data', color: 'blue' },
-  ];
+const messageConfigs = [
+    { object: miner_message, prefix: chalk.cyan('[Miner]') },
+    { object: server_message, prefix: chalk.magenta('[Server]') },
+    { object: data_message, prefix: chalk.blue('[Data]') },
+];
   
-  // Create objects for miner, server, and data messages using a loop
-  for (const type of messageTypes) {
-    for (const { object, prefix, color } of messageConfigs) {
-      object[type] = (message) => {
-        console.log(generateLogMessage(color, message, type === 'log' ? 'reset' : type));
-      };
+for (const type of messageTypes) {
+    for (const { object, prefix } of messageConfigs) {
+        object[type] = (message) => {
+            console.log(generateLogMessage(prefix, message, type === 'log' ? 'reset' : type));
+        };
     }
-  }
+}
 
 module.exports = {
     processRootDirectory,
