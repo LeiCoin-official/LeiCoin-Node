0d33e24ca2b4ce9dd5fb56f4707336f02379117e
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-14 17:18:26 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 
 const transactionRouter = require('./routes/sendTransactions')
