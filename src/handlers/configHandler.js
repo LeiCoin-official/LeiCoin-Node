0539247b6166c089cddafa38be7b0ecfc435190e
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-15 10:35:01 +0200

    update

--- src/handlers/configHandler.js
+++ src/handlers/configHandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 const util = require('../utils');
+const dotenv = require('dotenv');
 
 // Function to load the configuration file or a default if it doesn't exist
 function loadNormalConfig() {
@@ -30,7 +31,7 @@ function loadNormalConfig() {
             return loadConfig();
         }
     } catch (error) {
-        util.data_message.error('Error loading configuration:', error);
+        util.data_message.error('Error loading config configuration:', error);
         process.exit(1);
     }
 }
@@ -54,15 +55,42 @@ function loadKnownNodesConfig() {
             return loadKnownNodesConfig();
         }
     } catch (error) {
-        util.data_message.error('Error loading configuration:', error);
+        util.data_message.error('Error loading peers configuration:', error);
+        process.exit(1);
+    }
+}
+
+function loadENVConfig() {
+    const envFilePath = path.join(util.processRootDirectory, '/config/.env');
+    const defaultENVFilePath = path.join(util.processRootDirectory, '/config/sample.env');
+
+    try {
+        // Check if the configuration file exists
+        if (fs.existsSync(envFilePath)) {
+            // If it exists, read and parse the configuration
+            dotenv.config({ path: envFilePath });
+
+            const envData = {};
+
+            for (let key in process.env) {
+                envData[key] = process.env[key];
+            }
+
+            return envData;
+        } else {
+            // If it doesn't exist, read and parse the default configuration
+            const defaultENVData = fs.readFileSync(defaultENVFilePath, 'utf-8');
+            fs.writeFileSync(configFilePath, defaultENVData);
+            return loadKnownNodesConfig();
+        }
+    } catch (error) {
+        util.data_message.error('Error loading .env configuration:', error);
         process.exit(1);
     }
 }
 
 function loadConfig() {
-    let config;
-
-    config = loadNormalConfig();
+    const config = { ...loadNormalConfig(), ...loadENVConfig() };
 
     // Check for --internal-port and extract the value
     let internalPort = getInternalPort();
