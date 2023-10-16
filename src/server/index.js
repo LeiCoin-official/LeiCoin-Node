978ad680fe5f1c7bbd8666ce1fb8677079d9996f
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-16 16:58:30 +0200

    update

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const expressWS = require('express-ws');
 const bodyParser = require('body-parser');
 const config = require('../handlers/configHandler');
 const cors = require('cors');
 const util = require('../utils');
+const WebSocket = require('ws');
 
 const app = express();
+
+expressWS(app);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,8 +22,43 @@ app.use(function(req, res, next) {
 const transactionRouter = require('./routes/sendTransactions')
 const blocksRouter = require('./routes/sendBlocks');
 
-app.use('/sendtransactions', transactionRouter);
-app.use('/sendblocks', blocksRouter);
+app.use('/api/sendtransactions', transactionRouter);
+app.use('/api/sendblocks', blocksRouter);
+
+// Create a route that upgrades to WebSocket
+app.ws('/ws', (ws, req) => {
+    // Handle WebSocket connections here
+  
+    // Listen for messages from the client
+    ws.on('message', (message) => {
+      console.log('Received:', message);
+  
+      // Send a response back to the client
+      ws.send('Hello, WebSocket client!');
+    });
+  
+    // Handle WebSocket disconnections
+    ws.on('close', () => {
+      console.log('WebSocket connection closed.');
+    });
+  
+    // Connect to other servers on server startup
+    otherServers.forEach((serverURL) => {
+      const wsclient = new WebSocket(serverURL);
+  
+      wsclient.on('open', () => {
+        console.log(`Connected to: ${serverURL}`);
+      });
+  
+      wsclient.on('message', (message) => {
+        console.log(`Received from ${serverURL}: ${message}`);
+      });
+  
+      wsclient.on('close', () => {
+        console.log(`Connection to ${serverURL} closed.`);
+      });
+    });
+  });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
