074dc943fbdbccbb70b8db9bfc5d44f5471336a7
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-23 15:05:50 +0200

    Update sendTransactions.js

--- src/server/routes/api/sendTransactions.js
+++ src/server/routes/api/sendTransactions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const validation = require('../../validation');
+const validation = require('../../../validation');
 
 const data = require("../../handlers/dataHandler");
 
