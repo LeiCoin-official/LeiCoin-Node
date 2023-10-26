9e72f4042cc41d215a4e4d606e2340d5a922522c
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-26 15:53:51 +0200

    Update getUTXOS.js

--- src/server/routes/api/getUTXOS.js
+++ src/server/routes/api/getUTXOS.js
@@ -11,31 +11,40 @@ router.use('/', (req, res, next) => {
         return;
     }
 
-	const address = req.query.address;
-	
-	// Validate the transaction (add your validation logic here)
-	const utxos_reading_result = data.readUTXOS(address, txid, index);
+    const address = req.query.address;
+    const txid = req.query.txid || null; // Set to null if not provided
+    const index = req.query.index || null; // Set to null if not provided
 
-	if (utxos_reading_result.cb !== "sucess") {
-		if (utxos_reading_result.cb === "none") {
+    // Check if the address is provided
+    if (!address) {
+        res.status(400);
+        res.json({ message: 'Bad Request. Address is required.' });
+        return;
+    }
+
+    // Validate the transaction (add your validation logic here)
+    const utxos_reading_result = data.readUTXOS(address, txid, index);
+
+    if (utxos_reading_result.cb !== 'success') {
+        if (utxos_reading_result.cb === 'none') {
             res.status(400);
-            if (txid === null && index === 0) {
-                res.json({ message: "Bad Request. Address does not have UTXOS." });
+            if (!txid && !index) {
+                res.json({ message: 'Bad Request. Address does not have UTXOs.' });
                 return;
             }
-            res.json({ message: "Bad Request. UTXO was not found for the specified address." });
+            res.json({ message: 'Bad Request. UTXO was not found for the specified address.' });
             return;
-        }
+        } 
         res.status(500);
-        res.json({ message: "Internal Server Error. UTXOS could not be read for the given data." });
-        return;
-	}
-
+        res.json({ message: 'Internal Server Error. UTXOs could not be read for the given data.' });
+        return; 
+    }
     res.status(200);
     res.json({ data: utxos_reading_result.data });
     return;
-
 });
+
+
 
 // Replace this function with your actual transaction validation logic
 
