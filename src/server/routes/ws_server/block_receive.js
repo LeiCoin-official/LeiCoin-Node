a74b09cd1130fc2a47323018ebbcb4af83e24399
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2023-10-27 19:24:33 +0200

    update

--- src/server/routes/ws_server/block_receive.js
+++ src/server/routes/ws_server/block_receive.js
@@ -3,26 +3,30 @@ const util = require('../../../utils');
 const validation = require('../../../validation');
 
 module.exports = function (block) {
-    if (validation.isValidBlock(block).cb) {
+	const blocksExist = existsBlock(hash, index);
+	if (blocksExist.cb === "success" && !block.exists) {
 
-		writeBlock(block);
-		updateLatestBlockInfo(block.index, block.hash);
-		clearMempool(block);
+		if (validation.isValidBlock(block).cb) {
 
-		addUTXOS({txid: block.hash, index: 0, recipientAddress: block.coinbase.minerAddress, amount: block.coinbase.amount}, true);
-
-		for (const [, transactionData] of Object.entries(block.transactions)) {
-			deleteUTXOS(transactionData);
-			addUTXOS(transactionData, false);
+			writeBlock(block);
+			updateLatestBlockInfo(block.index, block.hash);
+			clearMempool(block);
+	
+			addUTXOS({txid: block.hash, index: 0, recipientAddress: block.coinbase.minerAddress, amount: block.coinbase.amount}, true);
+	
+			for (const [, transactionData] of Object.entries(block.transactions)) {
+				deleteUTXOS(transactionData);
+				addUTXOS(transactionData, false);
+			}
+	
+			util.ws_client_message.success(`Received block with hash ${block.hash} has been validated. Adding to Blockchain.`);
+		} else {
+			util.ws_client_message.error(`Received block with hash ${block.hash} is invalid.`);
 		}
 
-		util.ws_client_message.success(`Received block with hash ${block.hash} has been validated. Adding to Blockchain.`);
-	} else {
-		util.ws_client_message.error(`Received block with hash ${block.hash} is invalid.`);
+		return { alreadyExists: false};
 	}
-	const blocksExist = existsBlock(hash, index);
-	if (blocksExist.cb === "success") {
-		return { alreadyExists: blocksExist.exists};
-	}
-	return { alreadyExists: false};
+
+	util.ws_client_message.error(`Received block with hash ${block.hash} is invalid.`);
+	return { alreadyExists: true};
 }
