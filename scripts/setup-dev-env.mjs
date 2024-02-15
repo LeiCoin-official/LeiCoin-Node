04aa5419fdc41b1c7b29cdecdee1ae292b1f534d
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2024-02-15 14:29:53 +0100

    created setup dev env script

---
+++ scripts/setup-dev-env.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+function createVSCodeFolder() {
+    const vscodeFolderPath = path.join(process.cwd(), '.vscode');
+    if (!fs.existsSync(vscodeFolderPath)) {
+        fs.mkdirSync(vscodeFolderPath);
+        console.log('.vscode folder created successfully.');
+    } else {
+        console.log('.vscode folder already exists.');
+    }
+}
+
+function copyLaunchJsonFile() {
+    const sourceFilePath = path.join(process.cwd(), 'scripts', 'assets', 'launch.json');
+    const destinationFilePath = path.join(process.cwd(), '.vscode', 'launch.json');
+
+    fs.copyFileSync(sourceFilePath, destinationFilePath);
+    console.log('launch.json file copied successfully.');
+}
+
+function main() {
+    createVSCodeFolder();
+    copyLaunchJsonFile();
+}
+
+main();
