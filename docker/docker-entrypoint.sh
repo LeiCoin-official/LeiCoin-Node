2f6885a6b818d4e464c588befc0f4d9ba7733620
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-02-19 18:09:59 +0100

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,8 +5,29 @@ cd /home/container
 
 echo "Starting..."
 
+if [ -n "$STARTUP" ] && [[ "$STARTUP" == "start"* ]]; then
+    # Use parameter expansion to extract all content after "start"
+    args_after_start="${STARTUP#start }"
+
+    # Replace placeholders in args_after_start using eval and sed
+    MODIFIED_STARTUP=$(eval echo $(echo ${args_after_start} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+
+    args = $MODIFIED_STARTUP
+else
+    args = "$@"
+fi
+
+experimental = false
+branch_to_clone = "main"
+
+if [[ "$args" == *"--experimental"* ]]; then
+    echo "Experimental mode is active!"
+    experimental = true
+    branch_to_clone = "addfork"
+fi
+
 # Clone the GitHub repo to /home/container/tmp with the "docker" branch
-git clone --single-branch --branch main https://github.com/LeiCraft/LeiCoin-Node.git /home/container/gittmp
+git clone --single-branch --branch $branch_to_clone https://github.com/LeiCraft/LeiCoin-Node.git /home/container/gittmp
 
 # Check the exit code of the git clone command
 clone_exit_code=$?
@@ -25,23 +46,7 @@ fi
 rm -rf /home/container/gittmp
 
 # Install Node Packges
-
 npm i
 
-# Extract the value of --internal-port from STARTUP if it exists
-
-if [ -n "$STARTUP" ] && [[ "$STARTUP" == "start"* ]]; then
-    # Use parameter expansion to extract all content after "start"
-    args_after_start="${STARTUP#start }"
-
-    # Replace placeholders in args_after_start using eval and sed
-    MODIFIED_STARTUP=$(eval echo $(echo ${args_after_start} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-
-    # Pass the modified arguments to node index.js
-    node index.js $MODIFIED_STARTUP
-else
-    # If the STARTUP condition is not met, pass all command line arguments directly to node index.js
-    node index.js "$@"
-
-    tail -f /dev/null
-fi
+# Start the Script
+node index.js $args
