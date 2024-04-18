75699fe2172ce3201567ae9b67f1e2db5c0fb8fc
LeiCraft <linusfisscher08@gmx.net>
2024-04-18 12:20:45 +0200

    update

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -7,10 +7,10 @@ await esbuild.build({
     platform: "node",
     target: ["node18"],
     packages: "external",
-    external: Object.keys(dependencies),
+    //external: Object.keys(dependencies),
     bundle: true,
     sourcemap: true,
     format: 'cjs',
-    tsconfig: "tsconfig.json",
+    //tsconfig: "tsconfig.json",
     outfile: 'build/bin/leicoin-node.cjs'
 });
