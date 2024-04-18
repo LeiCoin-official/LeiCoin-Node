75c56ae631bc4c9c6dea17eb81c58b67ea595061
LeiCraft <88381277+LeiCraft@users.noreply.github.com>
2024-04-18 17:07:52 +0200

    added withEntryPoint

---
+++ scripts/build/withEntryPoint.js
@@ -0,0 +1,17 @@
+import { build } from 'esbuild';
+//import fs from 'fs';
+//const { dependencies } = JSON.parse(fs.readFileSync("./package.json"));
+
+await build({
+    entryPoints: ['scripts/build/entrypoint.ts'],
+    platform: "node",
+    target: ["node18"],
+    packages: "external",
+    //external: Object.keys(dependencies),
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    format: 'cjs',
+    //tsconfig: "tsconfig.json",
+    outfile: 'build/bin/leicoin-node-full.cjs'
+});
