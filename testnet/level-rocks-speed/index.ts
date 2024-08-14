2a4d2fbdd8ad00d00e398a3bc3507f416d2f59d7
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-08-14 20:59:47 +0200

    delete /src/storage/rocksdb/index.ts

--- testnet/level-rocks-speed/index.ts
+++ testnet/level-rocks-speed/index.ts
@@ -1,6 +1,107 @@
+import { Uint } from "../../src/utils/binary.js";
+import { LevelDB, RocksDB } from "./dbs.js";
 
-function createDB() {
-    const level = require('');
+
+function createRandomData() {
+    const data = new Uint8Array(1024);
+    crypto.getRandomValues(data);
+    return Uint.from(data);
 }
 
+class TestRunner<T extends LevelDB | RocksDB> {
+
+    private name: string;
+    private location: string;
+    private runCount: number = 0;
+
+    private db: T;
+
+    constructor(DBCLS: new (location: string) => T, name: string) {
+        this.name = name;
+        this.location = (__dirname + "/" + name.toLowerCase());
+
+        console.log(`[${this.name}] Creating database...`);
+        this.db = new DBCLS(this.location);
+    }
+
+    public async open() {
+        console.log(`[${this.name}] Opening database...`);
+        await this.db.open();
+    }
+
+    public async run(name: string, count: number) {
+        console.log(`[${name}] Running tests...`);
+
+        this.runCount = count;
+
+        await this.testWrite();
+        await this.testRead();
+    
+        await this.closeAndDelete();
+    
+        console.log(`[${name}] Finished tests`);
+    }
+
+    public async closeAndDelete() {
+        await this.db.close();
+        RocksDB.destroy(this.location, () => {});
+    }
+
+    private async testWrite() {
+        const name = `[${this.name}] Write ${this.runCount.toLocaleString()} entries`;
+
+        console.time(name);
+    
+        const promises: Promise<void>[] = [];
+    
+        for (let i = 0; i < this.runCount; i++) {
+            const key = Uint.from(i);
+            const value = createRandomData();
+    
+            promises.push(this.db.put(key, value));
+        }
+    
+        await Promise.all(promises);
+
+        console.timeEnd(name);
+    }
+    
+    private async testRead() {
+        const name = `[${this.name}] Read ${this.runCount.toLocaleString()} entries`;
+
+        console.time(name);
+    
+        const promises: Promise<void>[] = [];
+    
+        for (let i = 0; i < this.runCount; i++) {
+            const key = Uint.from(i);
+            promises.push(this.db.get(key));
+        }
+    
+        await Promise.all(promises);
+
+        console.timeEnd(name);
+    }    
+
+}
+
+async function runAll<T extends LevelDB | RocksDB>(
+    DBCLS: new (location: string) => T,
+    name: string,
+    count: number
+) {
+    const runner = new TestRunner(DBCLS, name);
+    await runner.open();
+    await runner.run(name, count);
+    await runner.closeAndDelete();
+}
+
+async function main() {
+    const count = 10_000;
+
+    await runAll(RocksDB, "RocksDB", count); 
+    await runAll(LevelDB, "LevelDB", count);
+}
+
+await main();
 
