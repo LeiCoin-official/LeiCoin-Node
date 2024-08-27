fb7e297c72094872828775f3db53d91b2a8a9b5b
LeiCraft <linusfisscher08@gmx.net>
2024-08-27 13:18:39 +0200

    update

--- src/storage/leveldb.ts
+++ src/storage/leveldb.ts
@@ -1,4 +1,5 @@
 import { DatabaseOptions, GetOptions, Level } from "level";
+//const { DatabaseOptions, GetOptions, ClassicLevel: Level } = require("../../node_modules/classic-level/prebuilds/linux-x64/node.napi.glibc.node");
 import { LevelDBEncoders } from "../encoding/leveldbEncoders.js";
 import { Uint } from "../binary/uint.js";
 
