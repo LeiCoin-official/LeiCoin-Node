c6119fd3a9af429d30936d11800b892d57485394
LeiCraft <linusfisscher08@gmx.net>
2024-08-29 16:16:55 +0200

    updated compiler help cmd

--- scripts/compile/commands/help.ts
+++ scripts/compile/commands/help.ts
@@ -3,7 +3,7 @@ import { Platforms } from "../compiler.js";
 
 export const HelpCMD = new class HelpCMD extends Command {
     async run(args: string[]) {
-        console.log("Usage: bun compile [<platform> | auto | all] [version]");
+        console.log("Usage: bun compile [<platform> | auto | all] [<version>] [--no-version-tag]");
         console.log("Platforms: " + Object.keys(Platforms).join(", "));
     }
 }();
