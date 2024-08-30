f9f4088cab2a2f7a541dc237ef330a15eed462f1
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-08-30 18:58:25 +0200

    introduced some bugs releated to the api

--- src/http_api/router.ts
+++ src/http_api/router.ts
@@ -1,12 +1,22 @@
 import Elysia, { Handler } from "elysia";
+import { InlineHandler } from "elysia/dist/types";
 
 export abstract class HTTPRoute {
     abstract path: string;
 
-    async get(handler: Handler = {set}) {
+    async get(...args[]: typeof Elysia.prototype.get.) {
+        return;
         
     }
 
+}
+
+function hi({handler, request, response}: InlineHandler<Schema, Singleton & {
+    derive: Ephemeral['derive'] & Volatile['derive'];
+    resolve: Ephemeral['resolve'] & Volatile['resolve'];
+}, JoinPath<BasePath, Path>) {
+    handler.set(response, 200, "Hi");
+            
 }
 
 export abstract class HTTPSubRouter extends HTTPRoute {
@@ -17,7 +27,7 @@ export abstract class HTTPSubRouter extends HTTPRoute {
 
     constructor(app: Elysia) {
         super();
-        this.app = app.get("/hi", ({set}) => {
+        this.app = app.get("/", ({server}) => {
             
         });
     }
@@ -25,6 +35,7 @@ export abstract class HTTPSubRouter extends HTTPRoute {
 }
 
 export class HTTPRootRouter extends HTTPSubRouter {
+    path: string;
     
 
 
