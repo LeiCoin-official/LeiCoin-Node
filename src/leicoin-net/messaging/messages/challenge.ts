fb64eac5a7c1a4c59b0bab41d46d76107891a5f8
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-11-30 19:08:05 +0000

    added challenge req msg

--- src/leicoin-net/messaging/messages/challenge.ts
+++ src/leicoin-net/messaging/messages/challenge.ts
@@ -1,22 +1,25 @@
-import { Uint256 } from "low-level";
+import { Uint256, Uint32 } from "low-level";
 import { BE, DataEncoder } from "../../../encoding/binaryEncoders.js";
 import { type PeerSocket } from "../../socket.js";
-import { LNMsgRequestHandler } from "../abstractMsgHandler.js";
+import { LNMsgDefaultHandler, LNMsgRequestHandler } from "../abstractMsgHandler.js";
 import { LNAbstractMsgBody, LNMsgID } from "../abstractMsg.js";
 
 export class ChallengeMsg extends LNAbstractMsgBody {
 
     constructor(
+        readonly id: Uint32,
         readonly challenge: Uint256
     ) {super()}
 
     protected static fromDict(obj: Dict<any>) {
         return new ChallengeMsg(
+            obj.id,
             obj.challenge
         )
     }
 
     protected static readonly encodingSettings: DataEncoder[] = [
+        BE(Uint32, "id"),
         BE(Uint256, "challenge")
     ]
 }
@@ -25,6 +28,32 @@ export namespace ChallengeMsg {
     export const ID = LNMsgID.from("77a9"); // CHALLENGE
 
     export const Handler = new class Handler extends LNMsgRequestHandler {
+        async receive(data: ChallengeMsg, socket: PeerSocket) {
+            return null;
+        }
+    }
+}
+
+export class ChallengeREQMsg extends LNAbstractMsgBody {
+
+    constructor(
+        readonly id: Uint32
+    ) {super()}
+
+    protected static fromDict(obj: Dict<any>) {
+        return new ChallengeREQMsg(obj.id)
+    }
+
+    protected static readonly encodingSettings: DataEncoder[] = [
+        BE(Uint32, "id")
+    ]
+    
+}
+
+export namespace ChallengeREQMsg {
+    export const ID = LNMsgID.from("77a9"); // CHALLENGE
+
+    export const Handler = new class Handler extends LNMsgDefaultHandler {
         async receive(data: ChallengeMsg, socket: PeerSocket) {
             return null;
         }
