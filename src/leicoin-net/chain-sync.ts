5826ece26d164b8b99f8cf68d7f07bc104cba7b1
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2024-12-10 19:33:53 +0100

    update

--- src/leicoin-net/chain-sync.ts
+++ src/leicoin-net/chain-sync.ts
@@ -34,7 +34,11 @@ export class NetworkSyncManager {
 
     private static async getRemoteBlock(socket: PeerSocket, index: Uint64) {
 
+        
+
         socket.request<BlocksMsg>(new GetBlocksMsg(index, Uint64.from(512)));
+
+
 
     }
 
