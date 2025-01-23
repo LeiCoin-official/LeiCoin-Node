bd47f861c86dbdcd555c3a3052f63dca89e96352
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-01-23 16:44:59 +0100

    updated http api

--- src/http_api/index.ts
+++ src/http_api/index.ts
@@ -2,6 +2,7 @@ import EventEmitter from "events";
 import { ModuleLike } from "@leicoin/utils/dataUtils";
 import Elysia from "elysia";
 import { cli } from "@leicoin/cli";
+import { NetworkUtils } from "@leicoin/utils/network-utils";
 
 export class HTTP_API implements ModuleLike<typeof HTTP_API> {
     public static initialized = false;
@@ -24,12 +25,17 @@ export class HTTP_API implements ModuleLike<typeof HTTP_API> {
         if (this.started) return;
         this.started = true;
 
+        const host = NetworkUtils.normalizeIP(config.host);
+        if (!host) {
+            throw new Error(`Invalid Hostname: ${host}`);
+        }
+
         this.app = this.app.listen({
-            port: config.port,
-            hostname: config.host,
+            hostname: host,
+            port: config.port
         });
         if (this.app.server) {
-            cli.api.info(`API listening on ${config.host}:${config.port}`);
+            cli.api.info(`API listening on ${NetworkUtils.formatAddress(host, config.port)}`);
         }
         if (config.eventHandler) {
             await this.setupEvents(config.eventHandler);
